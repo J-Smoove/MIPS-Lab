3d3eccc72a6187cc84d778106aa356ae7e902343
Josh Young <Joshua_Young@baylor.edu>
2017-02-01 16:21:21 -0600

    lab 3 stuff

---
+++ code/1_fetch/instr_mem_testbench.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/01/2017 03:58:27 PM
+// Design Name: 
+// Module Name: instr_mem_testbench
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module instr_mem_testbench(
+
+wire clock
+oscillator clk(clock);
+instr_mem mem_uut();
+
+initial
+
+begin
+
+end
+
+    );
+endmodule
